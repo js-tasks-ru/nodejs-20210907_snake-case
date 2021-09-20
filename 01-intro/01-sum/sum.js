595f10dba802c00e340cc7181fab683910c90043
CamelCase <admin@vetrov.site>
2021-09-20 17:56:33 +0300

    1st task

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,5 +1,13 @@
 function sum(a, b) {
-  /* ваш код */
+  
+  Object.values(arguments).forEach(argument => {
+    if ( ! isFinite( +argument ) ) {
+      throw new TypeError( argument + ' — не является числом!');
+    }
+  });
+  
+  return +a + +b;
+
 }
 
 module.exports = sum;
