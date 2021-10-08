809630ceeb312485a962386ee89802bfd503495c
CamelCase <admin@vetrov.site>
2021-10-09 02:09:09 +0300

    Исправил ошибку

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,12 +1,12 @@
 function sum(a, b) {
   
   for ( let arg of [a, b] ) {
-    if ( ! isFinite( +arg ) ) {
+    if ( typeof arg !== 'number' ) {
       throw new TypeError( arg + ' — не является числом!');
     }
   }
   
-  return +a + +b;
+  return a + b;
 
 }
 
